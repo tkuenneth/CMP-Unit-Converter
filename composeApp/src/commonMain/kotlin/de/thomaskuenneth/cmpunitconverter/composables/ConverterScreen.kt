6e2ccceb91448e91f305e3186ea72360b5268817
Thomas Künneth <kuennetht@gmail.com>
2025-03-29 13:29:43 +0100

    Update ConverterScreen.kt

--- composeApp/src/commonMain/kotlin/de/thomaskuenneth/cmpunitconverter/composables/ConverterScreen.kt
+++ composeApp/src/commonMain/kotlin/de/thomaskuenneth/cmpunitconverter/composables/ConverterScreen.kt
@@ -1,21 +1,17 @@
 package de.thomaskuenneth.cmpunitconverter.composables
 
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.SnackbarDuration
-import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.material3.*
 import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
 import androidx.compose.material3.adaptive.layout.PaneAdaptedValue
 import androidx.compose.material3.adaptive.layout.SupportingPaneScaffold
 import androidx.compose.material3.adaptive.layout.SupportingPaneScaffoldRole
 import androidx.compose.material3.adaptive.navigation.ThreePaneScaffoldNavigator
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import de.thomaskuenneth.cmpunitconverter.AbstractConverterViewModel
-import kotlinx.coroutines.launch
 import org.jetbrains.compose.resources.stringResource
 
 @OptIn(ExperimentalMaterial3AdaptiveApi::class, ExperimentalMaterial3Api::class)
@@ -25,45 +21,47 @@ fun ConverterScreen(
     viewModel: AbstractConverterViewModel,
     scrollBehavior: TopAppBarScrollBehavior
 ) {
-    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
-    val snackbarHostState = remember { SnackbarHostState() }
-    val scope = rememberCoroutineScope()
-    SupportingPaneScaffold(
-        directive = navigator.scaffoldDirective, mainPane = {
-            Converter(
-                viewModel = viewModel,
-                scrollBehavior = scrollBehavior,
-                shouldShowButton = navigator.scaffoldValue[SupportingPaneScaffoldRole.Supporting] == PaneAdaptedValue.Hidden
-            ) {
-                navigator.navigateTo(SupportingPaneScaffoldRole.Supporting)
-            }
-        }, supportingPane = {
-            with(viewModel.supportingPaneUseCase) {
-                val supportingPaneState by stateFlow.collectAsStateWithLifecycle()
-                SupportingPane(
-                    uiState = supportingPaneState,
-                    showUnits = navigator.scaffoldValue[SupportingPaneScaffoldRole.Main] == PaneAdaptedValue.Hidden,
-                    readMoreOnWikipedia = {
-                        openInBrowser(
-                            value = supportingPaneState.current,
-                            completionHandler = viewModel::handleOpenInBrowserResult
-                        )
-                    },
-                    clearConversionsHistory = ::clearConversionsHistory
-                )
-            }
-        }, value = navigator.scaffoldValue
-    )
-    uiState.snackbarVisibility.let { visibility ->
-        if (visibility is AbstractConverterViewModel.SnackbarVisibility.Message) {
-            val message = stringResource(visibility.message)
-            scope.launch {
-                snackbarHostState.showSnackbar(
-                    message = message,
-                    withDismissAction = true,
-                    duration = SnackbarDuration.Indefinite
-                )
-                viewModel.setSnackbarVisibility(AbstractConverterViewModel.SnackbarVisibility.Hidden)
+    val viewModelState by viewModel.uiState.collectAsStateWithLifecycle()
+    val snackbarHostState = remember(viewModelState) { SnackbarHostState() }
+    Scaffold(snackbarHost = { SnackbarHost(snackbarHostState) }) {
+        SupportingPaneScaffold(
+            directive = navigator.scaffoldDirective, mainPane = {
+                Converter(
+                    viewModel = viewModel,
+                    scrollBehavior = scrollBehavior,
+                    shouldShowButton = navigator.scaffoldValue[SupportingPaneScaffoldRole.Supporting] == PaneAdaptedValue.Hidden
+                ) {
+                    navigator.navigateTo(SupportingPaneScaffoldRole.Supporting)
+                }
+            }, supportingPane = {
+                with(viewModel.supportingPaneUseCase) {
+                    val supportingPaneState by stateFlow.collectAsStateWithLifecycle()
+                    SupportingPane(
+                        uiState = supportingPaneState,
+                        showUnits = navigator.scaffoldValue[SupportingPaneScaffoldRole.Main] == PaneAdaptedValue.Hidden,
+                        readMoreOnWikipedia = {
+                            openInBrowser(
+                                value = supportingPaneState.current,
+                                completionHandler = viewModel::handleOpenInBrowserResult
+                            )
+                        },
+                        clearConversionsHistory = ::clearConversionsHistory
+                    )
+                }
+            }, value = navigator.scaffoldValue
+        )
+        viewModelState.snackbarVisibility.let { visibility ->
+            if (visibility is AbstractConverterViewModel.SnackbarVisibility.Message) {
+                val message = stringResource(visibility.message)
+                LaunchedEffect(visibility) {
+                    snackbarHostState.showSnackbar(
+                        message = message,
+                        withDismissAction = true,
+                        duration = SnackbarDuration.Indefinite
+                    )
+                    snackbarHostState.currentSnackbarData?.dismiss()
+                    viewModel.setSnackbarVisibility(AbstractConverterViewModel.SnackbarVisibility.Hidden)
+                }
             }
         }
     }
