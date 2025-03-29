1d28261ff50d236180118fe915bf78e9997cb633
Thomas Künneth <kuennetht@gmail.com>
2025-03-29 14:34:51 +0100

    Refactoring - simplified code

--- composeApp/src/commonMain/kotlin/de/thomaskuenneth/cmpunitconverter/ScaffoldWithBackArrow.kt
+++ composeApp/src/commonMain/kotlin/de/thomaskuenneth/cmpunitconverter/ScaffoldWithBackArrow.kt
@@ -8,7 +8,6 @@ import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.unit.dp
 import de.thomaskuenneth.cmpunitconverter.app.AppViewModel
 import de.thomaskuenneth.cmpunitconverter.composeapp.generated.resources.*
 import org.jetbrains.compose.resources.stringResource
@@ -23,42 +22,38 @@ fun ScaffoldWithBackArrow(
     content: @Composable (PaddingValues, TopAppBarScrollBehavior) -> Unit
 ) {
     val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
-    if (shouldUseScaffold()) {
-        Scaffold(
-            contentWindowInsets = WindowInsets(0, 0, 0, 0),
-            topBar = {
-                CenterAlignedTopAppBar(
-                    navigationIcon = {
-                        if (shouldShowBack) IconButton(onClick = navigateBack) {
-                            Icon(
-                                Icons.AutoMirrored.Default.ArrowBack,
-                                contentDescription = stringResource(Res.string.back)
-                            )
-                        }
-                    },
-                    scrollBehavior = scrollBehavior,
-                    actions = {
-                        IconButton(onClick = { viewModel.setShouldShowSettings(true) }) {
-                            Icon(
-                                Icons.Default.Settings,
-                                contentDescription = stringResource(Res.string.settings_short)
-                            )
-                        }
-                        IconButton(onClick = { viewModel.setShouldShowAbout(true) }) {
-                            Icon(
-                                Icons.Default.Info,
-                                contentDescription = stringResource(Res.string.about_short)
-                            )
-                        }
-                    },
-                    title = { Text(text = stringResource(Res.string.app_name)) })
-            }) { innerPadding ->
-            content(innerPadding, scrollBehavior)
+    Scaffold(
+        contentWindowInsets = WindowInsets(0, 0, 0, 0),
+        topBar = {
+            if (shouldUseScaffold()) CenterAlignedTopAppBar(
+                navigationIcon = {
+                    if (shouldShowBack) IconButton(onClick = navigateBack) {
+                        Icon(
+                            Icons.AutoMirrored.Default.ArrowBack,
+                            contentDescription = stringResource(Res.string.back)
+                        )
+                    }
+                },
+                scrollBehavior = scrollBehavior,
+                actions = {
+                    IconButton(onClick = { viewModel.setShouldShowSettings(true) }) {
+                        Icon(
+                            Icons.Default.Settings,
+                            contentDescription = stringResource(Res.string.settings_short)
+                        )
+                    }
+                    IconButton(onClick = { viewModel.setShouldShowAbout(true) }) {
+                        Icon(
+                            Icons.Default.Info,
+                            contentDescription = stringResource(Res.string.about_short)
+                        )
+                    }
+                },
+                title = { Text(text = stringResource(Res.string.app_name)) })
+        }) { innerPadding ->
+        content(innerPadding, scrollBehavior)
+        BackHandler(shouldShowBack) {
+            navigateBack()
         }
-    } else {
-        content(PaddingValues.Absolute(0.dp, 0.dp, 0.dp, 0.dp), scrollBehavior)
-    }
-    BackHandler(shouldShowBack) {
-        navigateBack()
     }
 }
