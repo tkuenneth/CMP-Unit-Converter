5c66888364855427f54f4dd3d98560ce4b2773c1
Thomas Künneth <kuennetht@gmail.com>
2025-05-29 09:46:48 +0200

    Refactoring

--- composeApp/src/commonMain/kotlin/de/thomaskuenneth/cmpunitconverter/app/Settings.kt
+++ composeApp/src/commonMain/kotlin/de/thomaskuenneth/cmpunitconverter/app/Settings.kt
@@ -2,9 +2,8 @@ package de.thomaskuenneth.cmpunitconverter.app
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
@@ -20,6 +19,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import de.thomaskuenneth.cmpunitconverter.CheckboxWithLabel
 import de.thomaskuenneth.cmpunitconverter.composeapp.generated.resources.Res
 import de.thomaskuenneth.cmpunitconverter.composeapp.generated.resources.color_scheme
 import de.thomaskuenneth.cmpunitconverter.composeapp.generated.resources.dark
@@ -35,43 +35,41 @@ fun Settings(viewModel: AppViewModel = koinViewModel(), modifier: Modifier = Mod
     val uiState by viewModel.uiState.collectAsState()
     val onClick: (ColorSchemeMode) -> Unit = { viewModel.setColorSchemeMode(it) }
     Column(
-        modifier = modifier.padding(8.dp),
-        verticalArrangement = Arrangement.Top,
+        modifier = modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            text = stringResource(Res.string.color_scheme),
-            color = MaterialTheme.colorScheme.onSurface,
-            style = MaterialTheme.typography.titleMedium,
-        )
-        SingleChoiceSegmentedButtonRow {
-            SegmentedColorSchemeButton(
-                selected = uiState.colorSchemeMode == ColorSchemeMode.System,
-                unit = ColorSchemeMode.System,
-                onClick = onClick
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(
+                text = stringResource(Res.string.color_scheme),
+                color = MaterialTheme.colorScheme.onSurface,
+                style = MaterialTheme.typography.titleMedium,
             )
-            SegmentedColorSchemeButton(
-                selected = uiState.colorSchemeMode == ColorSchemeMode.Light,
-                unit = ColorSchemeMode.Light,
-                onClick = onClick
-            )
-            SegmentedColorSchemeButton(
-                selected = uiState.colorSchemeMode == ColorSchemeMode.Dark,
-                unit = ColorSchemeMode.Dark,
-                onClick = onClick
-            )
-        }
-        if (shouldShowExtendedAboutDialogCheckbox()) {
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Checkbox(
-                    uiState.showExtendedAboutDialog,
-                    onCheckedChange = { viewModel.setShowExtendedAboutDialog(!uiState.showExtendedAboutDialog) })
-                Text(
-                    text = stringResource(Res.string.show_extended_about_dialog),
-                    color = MaterialTheme.colorScheme.onSurface,
-                    style = MaterialTheme.typography.bodyMedium,
+            SingleChoiceSegmentedButtonRow {
+                SegmentedColorSchemeButton(
+                    selected = uiState.colorSchemeMode == ColorSchemeMode.System,
+                    unit = ColorSchemeMode.System,
+                    onClick = onClick
+                )
+                SegmentedColorSchemeButton(
+                    selected = uiState.colorSchemeMode == ColorSchemeMode.Light,
+                    unit = ColorSchemeMode.Light,
+                    onClick = onClick
+                )
+                SegmentedColorSchemeButton(
+                    selected = uiState.colorSchemeMode == ColorSchemeMode.Dark,
+                    unit = ColorSchemeMode.Dark,
+                    onClick = onClick
                 )
             }
+        }
+        if (shouldShowExtendedAboutDialogCheckbox()) {
+            CheckboxWithLabel(
+                label = stringResource(Res.string.show_extended_about_dialog),
+                checked = uiState.showExtendedAboutDialog
+            ) { checked -> viewModel.setShowExtendedAboutDialog(checked) }
         }
     }
 }
